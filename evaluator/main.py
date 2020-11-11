c049b2bbc72982637f3309db34c8984b51253fb5
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-11 04:22:49 +0100

    Made bot join any invite

--- evaluator/main.py
+++ evaluator/main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+import logging
 import asyncio
 import sys
 
-from nio import AsyncClientConfig, AsyncClient
+from nio import AsyncClientConfig, AsyncClient, InviteMemberEvent, JoinError
 
 from hopfenmatrix.run import run
 from evaluator.config import Config
+
+logger = logging.getLogger(__name__)
 
 
 async def main():
@@ -32,6 +35,28 @@ async def main():
         store_path=config.store_path,
         config=client_config,
     )
+
+    async def join(room, event):
+        logger.debug(f"Got invite to {room.room_id} from {event.sender}.")
+
+        # Attempt to join 3 times before giving up
+        for attempt in range(3):
+            result = await client.join(room.room_id)
+            if type(result) == JoinError:
+                logger.error(
+                    f"Error joining room {room.room_id} (attempt %d): %s",
+                    attempt,
+                    result.message,
+                )
+            else:
+                break
+        else:
+            logger.error("Unable to join room: %s", room.room_id)
+
+        # Successfully joined room
+        logger.info(f"Joined {room.room_id}")
+
+    client.add_event_callback(join, InviteMemberEvent)
 
     await run(client, config.user_id, config.user_password, config.device_name)
 
